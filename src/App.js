5d6095a33623f0f8cf8f53389d5dac4bc13a69ba
Sasha Putsyk <sashaputsyk100903gmail.com>
2023-07-16 16:31:30 +0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import User from './components/User';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <section>
+//     <User married={false} name="Арнольд Шварцнегер" avatar="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Arnold_Schwarzenegger_by_Gage_Skidmore_3.jpg/250px-Arnold_Schwarzenegger_by_Gage_Skidmore_3.jpg"/>
+//     <User married={true} name="Илон Маск" avatar="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQU2JRbbl3LBOm_an3eI5iplFhOoLESyBwUfmWDO49BS1EYuGUE"/>
+//     <User married={false} name="Сильвестер Сталлоне" avatar="https://t3.gstatic.com/licensed-image?q=tbn:ANd9GcTycuWyHFzuaSqgAWxYop7igeo5o_tDwQZYXpTHUuJ105VD0DoCozsp0FVsUdLhuOll"/>
+//     <User married={true} name="Райан Гослінг" avatar="https://t3.gstatic.com/licensed-image?q=tbn:ANd9GcQuW7VQCvyYXQZEG2aCYyTLeNmNjDyyw9K-IjoqNG2GXITFvNSQiBdBVhFjPdlf71sO"/>
+//     </section>
+//   );
+// }
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [
+        {
+          id: 1,
+          name: 'Арнольд Шварцнегер',
+          avatar: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Arnold_Schwarzenegger_by_Gage_Skidmore_3.jpg/250px-Arnold_Schwarzenegger_by_Gage_Skidmore_3.jpg',
+          married: false,
+        },
+        {
+          id: 2,
+          name: 'Илон Маск',
+          avatar: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQU2JRbbl3LBOm_an3eI5iplFhOoLESyBwUfmWDO49BS1EYuGUE',
+          married: true,
+        },
+        {
+          id: 3,
+          name: 'Сильвестер Сталлоне',
+          avatar: 'https://t3.gstatic.com/licensed-image?q=tbn:ANd9GcTycuWyHFzuaSqgAWxYop7igeo5o_tDwQZYXpTHUuJ105VD0DoCozsp0FVsUdLhuOll',
+          married: false,
+        },
+        {
+          id: 4,
+          name: 'Райан Гослінг',
+          avatar: 'https://t3.gstatic.com/licensed-image?q=tbn:ANd9GcQuW7VQCvyYXQZEG2aCYyTLeNmNjDyyw9K-IjoqNG2GXITFvNSQiBdBVhFjPdlf71sO',
+          married: true,
+        },
+      ]
+    }
+  }
+  render() {
+    const renderUsers = this.state.users.map(({ id, name, avatar, married }) =>
+      <li key={id}><User name={name} avatar={avatar} married={married} /></li>);
+
+    return (
+      <>
+        <h1>Users</h1>
+        <ul>{renderUsers}</ul>
+      </>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/User/index.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class User extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            buttonColorIndex: 0,
+            buttonColors: ['red', 'green'],
+        }
+    }
+
+    btnColor = () => {
+        // console.log('click');
+
+        const { buttonColorIndex, buttonColors } = this.state;
+        const nextColorIndex = (buttonColorIndex + 1) % buttonColors.length;
+        this.setState({ buttonColorIndex: nextColorIndex });
+    }
+
+    render() {
+        // console.log('render');
+
+        const { name, avatar, married } = this.props;
+        const { buttonColorIndex, buttonColors } = this.state;
+
+        const btnStyle = { backgroundColor: buttonColors[buttonColorIndex] };
+
+        return (
+            <article>
+                <button onClick={this.btnColor} style={btnStyle}>button</button>
+                <h2>{name}</h2>
+                <img src={avatar} alt='' />
+            </article>
+        );
+    }
+}
+
+export default User;
